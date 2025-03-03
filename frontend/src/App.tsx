0cbc6dc370f8f78ddd62e903543ad2b199825c9d
Bryce512 <bryce.whitworth@gmail.com>
2025-03-03 14:15:47 -0700

    add comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-
 import Schools from './CollegeBasketballTeams.json'
 
 function Header() {
@@ -8,7 +7,7 @@ function Header() {
     <h1>Welcome to the NCAA Basketball Database</h1>
   </>)
 }
-function SchoolCard({  
+function SchoolCard({  // Receives SchoolData from School list
   school,
   name,
   city,
@@ -35,7 +34,7 @@ function SchoolCard({
   );
 }
 
-function SchoolList() {
+function SchoolList() { // Creates a list of all School data
   return (
     <>
     <table className="centered-table">
@@ -47,7 +46,7 @@ function SchoolList() {
         </tr>
       </thead>
       <tbody> 
-          {Schools.teams.map((SchoolData) => (
+          {Schools.teams.map((SchoolData) => ( // Dynamically adds new card for each school
           <tr> <SchoolCard {...SchoolData} /> </tr>
           ))}
       </tbody>   
